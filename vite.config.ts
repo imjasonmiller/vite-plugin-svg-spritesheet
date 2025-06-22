1bd4ea36b2a640bc44a105f36772e6461ab382bc
Jason Miller <contact@jasonmiller.nl>
2025-06-22 19:35:11 +0200

    chore: fix publint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import { builtinModules } from 'module';
+import fs from 'fs/promises';
 import dts from 'unplugin-dts/vite';
 
 export default defineConfig({
@@ -20,7 +21,18 @@ export default defineConfig({
       },
     },
   },
-  plugins: [dts({ bundleTypes: true })],
+  plugins: [
+    dts({
+      bundleTypes: true,
+      afterBuild: async () => {
+        // To pass publint and have proper types for both ESM and CJS we have to
+        // duplicate them and add the correct export in package.json's `exports`.
+        //
+        // See: https://github.com/qmhc/vite-plugin-dts/issues/267
+        await fs.copyFile('dist/index.d.ts', 'dist/index.d.cts');
+      },
+    }),
+  ],
   test: {
     coverage: {
       provider: 'v8',
